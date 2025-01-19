9c594d4b759d6886fe112ada230704e079183287
qpaseo <qazcgik@gmail.com>
2025-01-19 19:32:22 +0900

    update : cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE,PATCH',
+    allowedHeaders: 'Content-Type, Authorization', // 허용할 헤더들
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
