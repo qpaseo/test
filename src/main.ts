9009851bfd00e200db0091c391c26fcdbf9818ae
qpaseo <qazcgik@gmail.com>
2025-01-19 21:23:25 +0900

    update : cors-9:23

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://web-umunjeong-server-m637n0dz9587ba58.sel4.cloudtype.app',
-    ],
-    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+  // cors 설정
+  app.use(
+    cors({
+      origin: [
+        'http://localhost:5173',
+        'https://web-umunjeong-server-m637n0dz9587ba58.sel4.cloudtype.app',
+      ],
+      methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+      allowedHeaders: 'Content-Type, Authorization',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
