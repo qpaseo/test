a67a44f59e70c36ff7f15d500b3f99239dde589f
qpaseo <qazcgik@gmail.com>
2025-01-20 03:29:32 +0900

    update : add enableCors-origin

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // cors 설정
-  app.use(
-    cors({
-      origin: [
-        'http://localhost:5173',
-        'https://web-umunjeong-server-m637n0dz9587ba58.sel4.cloudtype.app',
-      ],
-      methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
-      allowedHeaders: '*',
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
-    }),
-  );
+  // CORS 설정
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://umunjeong.netlify.app'],
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS', // 허용된 HTTP 메서드
+    allowedHeaders: 'Content-Type, Authorization', // 허용된 헤더
+    credentials: true, // 쿠키 전송 허용 (필요한 경우)
+    optionsSuccessStatus: 204, // OPTIONS 요청 성공 상태 코드
+  });
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+      );
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.status(204).send();
+    } else {
+      next();
+    }
+  });
 }
+
 bootstrap();
