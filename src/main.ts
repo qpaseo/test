b2a06105876b2ffa1ede739d80ab532bf6b6737d
qpaseo <qazcgik@gmail.com>
2025-01-19 19:45:20 +0900

    update : cors-OPTIONS

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: [
       'http://localhost:5173',
       'https://web-umunjeong-server-m637n0dz9587ba58.sel4.cloudtype.app',
     ],
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization', // 허용할 헤더들
+    allowedHeaders: 'Content-Type, Authorization',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
