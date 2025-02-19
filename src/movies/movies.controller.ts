6dd0087f5fb124ab1331756c64afae8795983eb2
qpaseo <qazcgik@gmail.com>
2025-02-19 18:29:08 +0900

    delete : ApiResponse 삭제

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -14,7 +14,7 @@ import { MoviesService } from './movies.service';
 import { Movie } from './entity/movie.entity';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @ApiTags('movies')
 @Controller('movies')
