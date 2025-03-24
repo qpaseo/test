e390e7613cf70c65c2a0239a22f8abe44aa6aff8
qpaseo <qazcgik@gmail.com>
2025-03-24 23:51:49 +0900

    update :: 챕터3 Icons and Transforms

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -134,6 +134,7 @@ class App extends StatelessWidget {
                 height: 20,
               ),
               Container(
+                clipBehavior: Clip.hardEdge,
                 decoration: BoxDecoration(
                   color: Color(0xFF1F2123),
                   borderRadius: BorderRadius.circular(25),
@@ -141,6 +142,7 @@ class App extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.all(24),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -177,6 +179,17 @@ class App extends StatelessWidget {
                             ],
                           )
                         ],
+                      ),
+                      Transform.scale(
+                        scale: 2.2,
+                        child: Transform.translate(
+                          offset: Offset(-5, 12),
+                          child: Icon(
+                            Icons.euro_rounded,
+                            color: Colors.white,
+                            size: 88,
+                          ),
+                        ),
                       )
                     ],
                   ),
