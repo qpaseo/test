8faef6d42739d6d9a5ea613224b463f9edc590e7
qpaseo <qazcgik@gmail.com>
2025-03-25 19:39:41 +0900

    update :: 챕터5 User Interface

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,99 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/widgets/Button.dart';
-import 'package:toonflix/widgets/currency_card.dart';
-
-class Player {
-  String? name; //있을수도 있고 아닐수도 있고
-
-  Player({required this.name});
-}
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
-  var seo = Player(name: "seo");
-  seo.name;
   runApp(App());
 }
 
-//StatefulWidget : 변하는 데이터가 있는 화면
-//StatelessWidget : 변하는 데이터가 없는 화면
-
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        textTheme: TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(
+              color: Color(0xFF232B55),
+            ),
           ),
-        ),
-      ),
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? MyLargeTitle() : const Text('notthing'),
-              IconButton(
-                  onPressed: toggleTitle, icon: Icon(Icons.remove_red_eye))
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  int count = 0;
-
-  //initState : state초기화
-  //build보다 먼저 있어야 함
-  //의존성 배열 없는 useEffect
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  //Widget이 정리될때 사용
-  //클린업 함수
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "My Large Title",
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+          cardColor: const Color(0xFFF4EDDB),
+          colorScheme: ColorScheme.light(
+            background: const Color(0xFFE7626C),
+          )),
+      home: const HomeScreen(),
     );
   }
 }
