09a46544262a5822dba1cc279cb45c82efd55e6b
qpaseo <qazcgik@gmail.com>
2025-03-25 13:15:03 +0900

    update :: state

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -14,11 +14,46 @@ void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    counter += 1;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "clock count",
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                counter.toString(),
+                style: TextStyle(fontSize: 30),
+              ), // 정수를 문자열로 변환하여 사용
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: Icon(Icons.add_box_rounded),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
