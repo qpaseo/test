dc14f84c62e1f1cb10d8b566e31ba6ee2798e45a
qpaseo <qazcgik@gmail.com>
2025-03-26 08:39:52 +0900

    update :: 챕터6 formJson

--- toonflix/lib/services/api_services.dart
+++ toonflix/lib/services/api_services.dart
@@ -1,15 +1,22 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon.model.dart';
 
 class ApiServices {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final res = await http.get(url);
     if (res.statusCode == 200) {
-      print(res.body);
-      return;
+      final List<dynamic> webToonList = jsonDecode(res.body);
+      for (var webtoon in webToonList) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     } else {
       throw Error();
     }
