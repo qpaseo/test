918aa59db6081539398b91fe859e8c16def5d3f9
qpaseo <qazcgik@gmail.com>
2025-03-26 23:24:42 +0900

    update :: 챕터6 Epidoses

--- toonflix/lib/screens/detail_screen.dart
+++ toonflix/lib/screens/detail_screen.dart
@@ -48,74 +48,125 @@ class _DetailScreenState extends State<DetailScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
             children: [
-              Hero(
-                tag: widget.id,
-                child: Container(
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 15,
-                          offset: Offset(10, 10),
-                          color: Colors.black.withOpacity(0.3),
-                        )
-                      ]),
-                  child: Image.network(
-                    widget.thumb,
-                    headers: const {
-                      'Referer': 'https://comic.naver.com',
-                    },
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: widget.id,
+                    child: Container(
+                      width: 250,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 15,
+                              offset: Offset(10, 10),
+                              color: Colors.black.withOpacity(0.3),
+                            )
+                          ]),
+                      child: Image.network(
+                        widget.thumb,
+                        headers: const {
+                          'Referer': 'https://comic.naver.com',
+                        },
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          FutureBuilder(
-            future: webtoon,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 50,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                        snapshot.data!.about,
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Text(
+              const SizedBox(
+                height: 25,
+              ),
+              FutureBuilder(
+                future: webtoon,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
                           style: TextStyle(
                             fontSize: 16,
                           ),
-                          '${snapshot.data!.genre} / ${snapshot.data!.age}'),
-                    ],
-                  ),
-                );
-              }
-              return Text("...");
-            },
-          )
-        ],
+                          snapshot.data!.about,
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                            '${snapshot.data!.genre} / ${snapshot.data!.age}'),
+                      ],
+                    );
+                  }
+                  return Text("...");
+                },
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              FutureBuilder(
+                future: episodes,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    //보여줄 내용이 많다면 ListView
+                    //많이 없다면 Column
+                    return Column(
+                      children: [
+                        for (var episcode in snapshot.data!)
+                          Container(
+                            margin: EdgeInsets.only(
+                              bottom: 20,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(
+                                color: Colors.green,
+                              ),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 20,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    episcode.title,
+                                    style: TextStyle(
+                                      color: Colors.green.shade400,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  const Icon(
+                                    Icons.chevron_right_outlined,
+                                    color: Colors.green,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                      ],
+                    );
+                  }
+                  return Container();
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
