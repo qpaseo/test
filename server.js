e4bfa4df1a1364adbf0365de25c49baa056c7257
qpaseo <qazcgik@gmail.com>
2025-03-28 08:26:32 +0900

    init :: 챕터 4 Scalar and Rooyt Types

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 import { ApolloServer, gql } from "apollo-server";
 
 // 1. GraphQL 스키마 정의 (보내는 쿼리)
+
 const typeDefs = gql`
-  type Query {
+  type User {
+    id: ID
+    username: String
+  }
+
+  type Tweet {
+    id: ID
     text: String
-    hello: String
+    author: User
+  }
+
+  type Query {
+    allTweets: [Tweet]
+    tweet(id: ID): Tweet
   }
 `;
 
 // 2. Resolvers (쿼리 처리 함수[서버가 받아 처리하는 로직])
 const resolvers = {
-  Query: {
-    text: () => "This is a text field",
-    hello: () => "Hello, GraphQL!",
-  },
+  Query: {},
 };
 
 // 3. Apollo Server 생성 (typeDefs와 resolvers 포함)
