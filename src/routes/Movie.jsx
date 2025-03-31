e42dfb3f933f9cbf8ded9b77262a577b87a96bbc
qpaseo <qazcgik@gmail.com>
2025-03-31 17:20:38 +0900

    챕터2 Apollo Dev Tools

--- src/routes/Movie.jsx
+++ src/routes/Movie.jsx
@@ -1,5 +1,26 @@
-const Movie = () => {
-  return <div></div>;
-};
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
 
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+      small_cover_image
+    }
+  }
+`;
+
+const Movie = () => {
+  const { id } = useParams();
+  const { data, loading } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: id,
+    },
+  });
+  if (loading) {
+    return <h1>loading movie</h1>;
+  }
+  return <div>{data.movie.title}</div>;
+};
 export default Movie;
--- src/routes/Movies.jsx
+++ src/routes/Movies.jsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 
 const GET_MOVIES = gql`
   query getAllMovies {
@@ -22,7 +23,9 @@ const Movies = () => {
     <ul>
       <h1>Movies</h1>
       {data.allMovies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
       ))}
     </ul>
   );
