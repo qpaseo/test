1878a362443434d3e02653e6a4438ae4afe8de19
qpaseo <qazcgik@gmail.com>
2025-03-31 10:58:04 +0900

    update :: 챕터4 migrating from rest to graphql

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
 const tweets = [
   {
@@ -54,12 +55,38 @@ const typeDefs = gql`
     allUsers: [User!]!
     tweet(id: ID!): Tweet
     ping: String!
+    allMovies: [Movie!]!
+    movie(id: String!): Movie
   }
 
   # 나머지 요청은 여기에
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet
     deleteTweet(id: ID!): Boolean!
+  }
+
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `;
 
@@ -77,6 +104,18 @@ const resolvers = {
 
     tweet(root, { id }) {
       return tweets.find((tweet) => tweet.id === id);
+    },
+
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((r) => r.json())
+        .then((json) => json.data.movies);
+    },
+
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then((r) => r.json())
+        .then((json) => json.data.movie);
     },
   },
   Mutation: {
