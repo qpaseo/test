0e632dafc63491e0319e3c924f4bf935d2ba4e39
qpaseo <qazcgik@gmail.com>
2025-03-31 18:58:48 +0900

    챕터3 update :: GraphQL

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import client from "./routes/apollo.js";
+import client from "./apollo.js";
 import { ApolloProvider } from "@apollo/client";
 
 createRoot(document.getElementById("root")).render(
