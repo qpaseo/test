1cf2fd2c1567eb1752f85b1cf9d214b7815734ca
qpaseo <qazcgik@gmail.com>
2026-04-08 21:54:00 +0900

    fix :: initializeDatabase 하기전에 initMonthlyFinancesJob 사용해서 오류 나던 부분 수정

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import app from "./app";
+import { initializeDatabase } from "./config/db";
 import { ENV } from "./config/env";
 import { initMonthlyFinancesJob } from "./jobs/monthlyFinancesJob";
 
 app.listen(ENV.PORT, async () => {
   console.log(`Server running on port ${ENV.PORT}`);
+  await initializeDatabase();
   await initMonthlyFinancesJob();
 });
